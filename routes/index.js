e4c0f46f55a0613df634489d5a66e43a10dfe95f
christijohnson <christiajohnson@gmail.com>
2024-01-20 18:08:28 -0700

    connected contacts

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
+// const routes = require('express').Router();
+// const lesson1Controller = require('../controllers/lesson1');
 
-routes.get('/', lesson1Controller.kevinRoute);
-routes.get('/kati', lesson1Controller.katiRoute);
+// routes.get('/', lesson1Controller.kevinRoute);
+// routes.get('/kati', lesson1Controller.katiRoute);
 
-module.exports = routes;
+// module.exports = routes;
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
