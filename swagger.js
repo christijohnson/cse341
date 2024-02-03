556939c5424d9a93c8be524b9eb52eceb7793a42
christijohnson <christiajohnson@gmail.com>
2024-02-03 11:36:55 -0700

    add render host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Contacts API'
   },
-  host: 'localhost:8080',
+  host: 'cse341-heni.onrender.com',
+  // host:  'localhost:8080',
   schemes: ['http']
 };
 
